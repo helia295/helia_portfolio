828e016e3b9a1f46b1de1fd5b83a5b1473f4c591
helia295 <helia.ldinh@gmail.com>
2026-07-24 16:14:44 -0400

    Add Docker Compose deployment setup

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ import datetime
 import os
 from flask import Flask, render_template, request, url_for
 from dotenv import load_dotenv
-from peewee import CharField, DateTimeField, Model, MySQLDatabase, TextField
+from peewee import CharField, DateTimeField, Model, MySQLDatabase, SqliteDatabase, TextField
 from playhouse.shortcuts import model_to_dict
 
 from .data import EDUCATION, EXPERIENCES, HOBBIES, HOBBY_SECTIONS, VISITED_PLACES
@@ -10,13 +10,17 @@ from .data import EDUCATION, EXPERIENCES, HOBBIES, HOBBY_SECTIONS, VISITED_PLACE
 load_dotenv()
 app = Flask(__name__)
 
-mydb = MySQLDatabase(
-    os.getenv("MYSQL_DATABASE"),
-    user=os.getenv("MYSQL_USER"),
-    password=os.getenv("MYSQL_PASSWORD"),
-    host=os.getenv("MYSQL_HOST"),
-    port=3306,
-)
+if os.getenv("TESTING") == "true":
+    print("Running in test mode")
+    mydb = SqliteDatabase('file:memory?mode=memory&cache=shared', uri=True)
+else:
+    mydb = MySQLDatabase(
+        os.getenv("MYSQL_DATABASE"),
+        user=os.getenv("MYSQL_USER"),
+        password=os.getenv("MYSQL_PASSWORD"),
+        host=os.getenv("MYSQL_HOST"),
+        port=3306,
+    )
 
 print(mydb)
 
